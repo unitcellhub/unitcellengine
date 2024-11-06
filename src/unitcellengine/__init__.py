20468f5442e1f6ac4b62ba218b1b8d6dec4c376c
Ryan Watkins <ryan.t.watkins@jpl.nasa.gov>
2024-11-06 12:30:39 -0800

    Bump version: 0.0.6-rc0 → 0.0.7-dev2

--- src/unitcellengine/__init__.py
+++ src/unitcellengine/__init__.py
@@ -6,4 +6,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 # from unitcellengine.design import UnitcellDesign, UnitcellDesigns
 
-__version__ = version = "0.0.6-rc0"
+__version__ = version = "0.0.7-dev2"
